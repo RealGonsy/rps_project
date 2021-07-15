7397463ffc923f5ea4abb5e734a1a9e6be31820a
pleissimanu <pirttisalom@gmail.com>
2021-07-15 12:35:43 +0300

    added working pics and function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import Image from 'react-bootstrap/Image'
-import { Navbar, Container, Col, Row } from 'react-bootstrap'
+import { Navbar, Container, Col, Row, Button } from 'react-bootstrap'
 import { useState } from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import testUtils from 'react-dom/test-utils'
 
 const App = () => {
   const [myChoice, setMyChoice] = useState('')
   const [score, setScore] = useState(0)
+  const [choice, setChoice] = useState('')
+
+  const imageClick = (event) => {
+    const name = event.target.getAttribute('data-name')
+    setChoice(`You have picked ${name} as your weapon`)
+
+  }
 
   return (
-    <Container>
-      <Row>
-        <Col xs={6} md={4}>
-          <Image src="rockk.jpg" rounded />
-        </Col>
-        <Col xs={6} md={4}>
-          <Image src="paper.png" roundedCircle />
-        </Col>
-        <Col xs={6} md={4}>
-          <Image src="scissors.png" thumbnail />
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <Container>
+        <h2>{choice}</h2>
+        <Row>
+          <Col md={4} style={{ textAlign: "center" }}>
+            <Image src="rock.png" data-name="rock" onClick={imageClick} fluid />
+            <p></p>
+            <h2>mo2</h2>
+          </Col>
+          <Col md={{ span: 4, offset: 4 }} style={{ textAlign: "center" }}>
+            <Image src="paper.png" data-name="paper" onClick={imageClick} fluid />
+            <p>mo</p>
+          </Col>
+        </Row>
+        <Row>
+          <Container>
+            <Col md={{ span: 6, offset: 3 }} style={{ textAlign: "center" }}>
+              <Image src="scissors.png" data-name="scissors" onClick={imageClick} fluid />
+            </Col>
+          </Container>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
